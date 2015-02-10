a3b2334a64591656b6eead67bc3d4cd32fc0c95b
fffw <fffw@getlantern.org>
2015-02-10 20:54:15 +0800

    add error checking and reporting for windows

--- systray_windows.c
+++ systray_windows.c
@@ -11,20 +11,40 @@ static NOTIFYICONDATA nid;
 static HWND hWnd;
 static HMENU hTrayMenu;
 
-wchar_t* UTF8ToUnicode(const char* str)
-{
-	int textlen;
-	wchar_t * result;
-	textlen = MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL ,0);
-	result = (wchar_t *)calloc((textlen+1), sizeof(wchar_t));
-	MultiByteToWideChar(CP_UTF8, 0, str, -1, (LPWSTR)result, textlen);
+void reportWindowsError(const char* action) {
+	LPTSTR pErrMsg = NULL;
+	DWORD errCode = GetLastError();
+	DWORD result = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|
+			FORMAT_MESSAGE_FROM_SYSTEM|
+			FORMAT_MESSAGE_ARGUMENT_ARRAY,
+			NULL,
+			errCode,
+			LANG_NEUTRAL,
+			pErrMsg,
+			0,
+			NULL);
+	printf("Systray error %s: %d %s\n", action, errCode, pErrMsg);
+}
+
+wchar_t* UTF8ToUnicode(const char* str) {
+	wchar_t* result;
+	int textLen = MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL ,0);
+	result = (wchar_t *)calloc((textLen+1), sizeof(wchar_t));
+	int converted = MultiByteToWideChar(CP_UTF8, 0, str, -1, (LPWSTR)result, textLen);
+	// Ensure result is alway zero terminated in case either syscall failed
+	if (converted == 0) {
+		reportWindowsError("convert UTF8 to UNICODE");
+		result[0] = L'\0';
+	}
 	return result;
 }
 
-void ShowMenu(HWND hWnd)
-{
+void ShowMenu(HWND hWnd) {
 	POINT p;
-	GetCursorPos(&p);
+	if (0 == GetCursorPos(&p)) {
+		reportWindowsError("get tray menu position");
+		return;
+	};
 	SetForegroundWindow(hWnd); // Win32 bug work-around
 	TrackPopupMenu(hTrayMenu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, p.x, p.y, 0, hWnd, NULL);
 
@@ -34,14 +54,15 @@ char* GetMenuItemId(int index) {
 	MENUITEMINFO menuItemInfo;
 	menuItemInfo.cbSize = sizeof(MENUITEMINFO);
 	menuItemInfo.fMask = MIIM_DATA;
-	GetMenuItemInfo(hTrayMenu, index, TRUE, &menuItemInfo);
+	if (0 == GetMenuItemInfo(hTrayMenu, index, TRUE, &menuItemInfo)) {
+		reportWindowsError("get menu item id");
+		return NULL;
+	}
 	return (char*)menuItemInfo.dwItemData;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
 		case WM_MENUCOMMAND:
 			systray_menu_item_selected(GetMenuItemId(wParam));
 			break;
@@ -50,8 +71,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			PostQuitMessage(0);
 			break;
 		case WM_SYSTRAY_MESSAGE:
-			switch(lParam)
-			{
+			switch(lParam) {
 				case WM_RBUTTONUP:
 					ShowMenu(hWnd);
 					break;
@@ -68,8 +88,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance, TCHAR* szWindowClass)
-{
+void MyRegisterClass(HINSTANCE hInstance, TCHAR* szWindowClass) {
 	WNDCLASSEX wcex;
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
@@ -85,15 +104,13 @@ ATOM MyRegisterClass(HINSTANCE hInstance, TCHAR* szWindowClass)
 	wcex.lpszClassName  = szWindowClass;
 	wcex.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
 
-	return RegisterClassEx(&wcex);
+	RegisterClassEx(&wcex);
 }
 
-HWND InitInstance(HINSTANCE hInstance, int nCmdShow, TCHAR* szWindowClass)
-{
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow, TCHAR* szWindowClass) {
 	HWND hWnd = CreateWindow(szWindowClass, TEXT(""), WS_OVERLAPPEDWINDOW,
 			CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-	if (!hWnd)
-	{
+	if (!hWnd) {
 		return 0;
 	}
 
@@ -104,25 +121,22 @@ HWND InitInstance(HINSTANCE hInstance, int nCmdShow, TCHAR* szWindowClass)
 }
 
 
-void createMenu()
-{
+BOOL createMenu() {
 	hTrayMenu = CreatePopupMenu();
 	MENUINFO menuInfo;
 	menuInfo.cbSize = sizeof(MENUINFO);
 	menuInfo.fMask = MIM_APPLYTOSUBMENUS | MIM_STYLE;
 	menuInfo.dwStyle = MNS_NOTIFYBYPOS;
-	SetMenuInfo(hTrayMenu, &menuInfo);
+	return SetMenuInfo(hTrayMenu, &menuInfo);
 }
 
-void addNotifyIcon()
-{
+BOOL addNotifyIcon() {
 	nid.cbSize = sizeof(NOTIFYICONDATA);
 	nid.hWnd = hWnd;
 	nid.uID = 100;
 	nid.uCallbackMessage = WM_SYSTRAY_MESSAGE;
 	nid.uFlags = NIF_MESSAGE;
-	Shell_NotifyIcon(NIM_ADD, &nid);
-
+	return Shell_NotifyIcon(NIM_ADD, &nid);
 }
 
 int nativeLoop(void) {
@@ -130,19 +144,16 @@ int nativeLoop(void) {
 	TCHAR* szWindowClass = TEXT("SystrayClass");
 	MyRegisterClass(hInstance, szWindowClass);
 	hWnd = InitInstance(hInstance, FALSE, szWindowClass); // Don't show window
-	if (!hWnd)
-	{
-		return;
+	if (!hWnd) {
+		return EXIT_FAILURE;
 	}
-
-	createMenu();
-	addNotifyIcon();
+	if (!createMenu() || !addNotifyIcon()) {
+		return EXIT_FAILURE;
+	}
 	systray_ready();
 
-	// Main message loop:
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
+	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}   
@@ -151,7 +162,6 @@ int nativeLoop(void) {
 
 
 void setIcon(const char* iconBytes, int length) {
-	// Let's load up the tray icon
 	HICON hIcon;
 	{
 		// This is really hacky, but LoadImage won't let me load an image from memory.
@@ -160,32 +170,26 @@ void setIcon(const char* iconBytes, int length) {
 		// From http://msdn.microsoft.com/en-us/library/windows/desktop/aa363875.aspx
 		TCHAR szTempFileName[MAX_PATH+1];
 		TCHAR lpTempPathBuffer[MAX_PATH+1];
-		int dwRetVal = GetTempPath(MAX_PATH+1,        // length of the buffer
-				lpTempPathBuffer); // buffer for path
-		if (dwRetVal > MAX_PATH+1 || (dwRetVal == 0))
-		{
-			return; // Failure
+		int dwRetVal = GetTempPath(MAX_PATH+1, lpTempPathBuffer);
+		if (dwRetVal > MAX_PATH+1 || (dwRetVal == 0)) {
+			reportWindowsError("get temp icon path");
+			return;
 		}
 
-		//  Generates a temporary file name.
-		int uRetVal = GetTempFileName(lpTempPathBuffer, // directory for tmp files
-				TEXT("_tmpicon"), // temp file name prefix
-				0,                // create unique name
-				szTempFileName);  // buffer for name
-		if (uRetVal == 0)
-		{
-			return; // Failure
+		int uRetVal = GetTempFileName(lpTempPathBuffer, TEXT("systray_"), 0, szTempFileName);
+		if (uRetVal == 0) {
+			reportWindowsError("get temp icon file name");
+			return;
 		}
 
-		// Dump the icon to the temp file
 		FILE* fIcon = _wfopen(szTempFileName, TEXT("wb"));
-		fwrite(iconBytes, 1, length, fIcon);
+		if (length != fwrite(iconBytes, 1, length, fIcon)) {
+			printf("error write temp icon file\n");
+		};
 		fclose(fIcon);
 
-		// Load the image from the file
 		hIcon = LoadImage(NULL, szTempFileName, IMAGE_ICON, 64, 64, LR_LOADFROMFILE);
 
-		// Delete the temp file
 		_wremove(szTempFileName);
 	}
 
@@ -229,6 +233,9 @@ void add_or_update_menu_item(char* menuId, char* ctitle, char* ctooltip, short d
 	int i;
 	for (i = 0; i < itemCount; i++) {
 		char * idString = GetMenuItemId(i);
+		if (NULL == idString) {
+			continue;
+		}
 		if (strcmp(menuId, idString) == 0) {
 			free(idString);
 			SetMenuItemInfo(hTrayMenu, i, TRUE, &menuItemInfo);
