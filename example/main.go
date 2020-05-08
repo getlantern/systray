922bf41e443908fbc59e1089495e79544d5ce6c7
Joesis <joesis@getlantern.org>
2020-05-08 00:37:02 -0700

    try integrate webview

--- example/main.go
+++ example/main.go
@@ -5,12 +5,21 @@ import (
 	"io/ioutil"
 	"time"
 
+	"github.com/zserge/webview"
+
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
 	"github.com/skratchdot/open-golang/open"
 )
 
 func main() {
+	debug := true
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetTitle("Minimal webview example")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+	w.Run()
 	onExit := func() {
 		fmt.Println("Starting onExit")
 		now := time.Now()
